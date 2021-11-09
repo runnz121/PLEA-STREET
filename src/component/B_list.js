0df0ddbc2aef9c796aa4c9a86383f855087b22a5
mingle-mongle <loveunhye95@gmail.com>
2021-11-10 05:27:09 +0900

    Feat: 추가 무한스크롤 테스트페이지

--- src/component/B_list.js
+++ src/component/B_list.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Comment from './Comment'
+import Loading from './Loading'
+import EndMsg from './EndMsg'
 
 function B_list() {
-    return (
-        <div>
-            
-        </div>
+
+  const [items, setItems] = useState([])
+
+  const [hasMore, setHasMore] = useState(true)
+
+  const [page, setPage] = useState(2)
+
+  useEffect(() => {
+    const getComments = async () => {
+
+      const res = await fetch(
+        // 'http://localhost:3004/comments?_page=1&_limit=20'
+        'https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20'
+      )
+      const data = await res.json()
+      setItems(data)
+    }
+
+    getComments()
+  }, [])
+
+  console.log(items)
+
+  const fetchComments = async () => {
+    const res = await fetch(
+      // `http://localhost:3004/comments?_page=${page}&_limit=20`
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=20`
     )
+    const data = await res.json()
+    return data
+  }
+
+  const fetchData = async () => {
+    const commentsFormServer = await fetchComments()
+
+    setItems([...items, ...commentsFormServer])
+
+    if (commentsFormServer.length === 0 || commentsFormServer.length < 20) {
+      setHasMore(false)
+    }
+
+    setPage(page + 1)
+
+  }
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length} //This is important field to render the next data
+      next={fetchData}
+      hasMore={hasMore}
+      loader={<Loading />}
+      endMessage={<EndMsg />}
+    >
+    {items.map((item) => {
+      return <Comment key={item.id} item={item} />
+           })}
+  </InfiniteScroll>
+  )
 }
 
 export default B_list
---
+++ src/component/Loading.js
@@ -0,0 +1,9 @@
+import { Loader } from 'semantic-ui-react'
+
+function Loading() {
+  return (
+    <Loader active inline='centered'>Loading</Loader>
+  )
+}
+
+export default Loading
