4087a55a6266b9d9e53a9ff2d0f5620d4c739869
mingle-mongle <loveunhye95@gmail.com>
2021-11-11 16:18:43 +0900

    Feat: global css/theme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
+import { ThemeProvider } from 'styled-components'
+import theme from './style/theme'
+import GlobalStyle from './style/global'
 
 // <React.StrictMode>
 //  </React.StrictMode>,
 ReactDOM.render(
- 
-       <App />
+    
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyle />
+      </ThemeProvider>
 , 
   document.getElementById('root')
 );
