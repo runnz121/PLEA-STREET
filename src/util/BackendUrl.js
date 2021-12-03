f55974b384ad18a1cbf34e47b564841880eae253
Hey E Sang Hae See <54499829+runnz121@users.noreply.github.com>
2021-12-03 12:56:05 +0900

    refector: local 주소를 heroku 주소로 변경

--- src/util/BackendUrl.js
+++ src/util/BackendUrl.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8080"
+// export const BACKEND_URL = "http://localhost:8080"
 
 //heroku
-// export const BACKEND_URL = "https://pleastreetbackend.herokuapp.com"
+export const BACKEND_URL = "https://pleastreetbackend.herokuapp.com"
